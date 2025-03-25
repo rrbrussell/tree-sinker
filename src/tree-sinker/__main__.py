73060ca3777cd1380221893ddefd5303a954a72b
root <root@helena.private.rrbrussell.com>
2025-03-25 16:42:31 -0500

    the starting of the project

---
+++ src/tree-sinker/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import configparser
+import sys
+
+config = configparser.ConfigParser()
+config['fetch.from'] = {'server': 'http://server.example.com', 'path': '/directory1/directory2'}
+config['store.into'] = {'repos_dir': '/var/db/repos'}
+config.write(sys.stdout);
