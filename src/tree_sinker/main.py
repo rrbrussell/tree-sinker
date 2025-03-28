dc112748aaf95771340fc65169928e847fbc7b42
Robert R. Russell <robert@rrbrussell.com>
2025-03-27 20:51:20 -0500

    The wheel builds. I don't know if it can be installed yet though.

--- src/tree_sinker/main.py
+++ src/tree_sinker/main.py
@@ -7,3 +7,8 @@ config = configparser.ConfigParser()
 config['fetch.from'] = {'server': 'http://server.example.com', 'path': '/directory1/directory2'}
 config['store.into'] = {'repos_dir': '/var/db/repos'}
 config.write(sys.stdout);
+
+
+def main_cli():
+    print("Hello you have called tree-sinker");
+
