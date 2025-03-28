bb04feac598387b4511322873d8ade59a9a17d4f
Robert R. Russell <robert@rrbrussell.com>
2025-03-28 00:09:24 -0500

    Starting work on the argument parser.

--- src/tree_sinker/__init__.py
+++ src/tree_sinker/__init__.py
@@ -1,5 +1,11 @@
+import argparse
+import sys
 
-def main_cli():
-    print("Hello you have called tree-sinker.\n");
-
+def main_cli(argv=sys.argv):
+    print(argv)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("repository_name", help="The repository name that you want me to syncronize.")
+    args = parser.parse_args()
+    print(args)
+    print(args.repository_name)
 # End of File
