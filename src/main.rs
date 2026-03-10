77b2e96623c68e467d1ce4ef33381558d3865b22
Robert R. Russell <robert@rrbrussell.com>
2026-03-09 20:19:45 -0500

    file metadata reader test complete.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::fs::File;
+use std::fs::Metadata;
+use std::io::prelude::*;
+use std::io::Error;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Test program.");
+    let temp = File::open("testfile");
+    let f: File;
+    match temp {
+        Ok(_f) => {
+            println!("testfile can be read.");
+            f = _f;
+        }
+        Err(e) => {
+            println!("{e:?}");
+            exit(1);
+        }
+    }
+    let temp = f.metadata();
+    let m: Metadata;
+    match temp {
+        Ok(_m) => {
+            println!("testfile's metadata can be read.");
+            m = _m;
+        }
+        Err(e) => {
+            println!("{e:?}");
+            exit(1);
+        }
+    }
+    println!("Testfile is {:x} bytes in length", m.len());
 }
